9fc6ea33d558feb61f91152ef1365bbbf51a73b8
uday <udaysah0003@gmail.com>
2024-05-19 02:26:29 +0530

    implement little bit css and logic for light chnges

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import roadImg from './assets/road.jpg'
+const TrafficLight = () => {
+  const [state, setState] = useState('RED');
 
-function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    let timer;
+    switch (state) {
+      case 'RED':
+        timer = setTimeout(() => setState('GREEN'), 20000); // 20 seconds
+        break;
+      case 'GREEN':
+        timer = setTimeout(() => setState('YELLOW'), 15000); // 15 seconds
+        break;
+      case 'YELLOW':
+        timer = setTimeout(() => setState('RED'), 5000); // 5 seconds
+        break;
+      default:
+        break;
+    }
+    return () => clearTimeout(timer);
+  }, [state]);
+
+  const handleClick = (newState) => {
+    setState(newState);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main-container">
+      <img src={roadImg} alt="road image" className='imgRoad' />
+      <div className="traffic-light">
+        <div className="light-head">
+          <div
+            id="red"
+            className={`light ${state === 'RED' ? 'red' : ''}`}
+            onClick={() => handleClick('RED')}
+          ></div>
+          <div
+            id="yellow"
+            className={`light ${state === 'YELLOW' ? 'yellow' : ''}`}
+            onClick={() => handleClick('YELLOW')}
+          ></div>
+          <div
+            id="green"
+            className={`light ${state === 'GREEN' ? 'green' : ''}`}
+            onClick={() => handleClick('GREEN')}
+          ></div>
+        </div>
+        <div className="traffic-pole"></div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    </div>
+  );
+};
+function App() {
+  return (
+    <div className="App">
+      <TrafficLight />
+    </div>
+  );
 }
 
-export default App
+export default App;
